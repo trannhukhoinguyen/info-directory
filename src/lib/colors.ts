c6158a4e6a81d117352ac3e92b5df3d827e22c6d
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2026-01-16 18:21:41 +0700

    update

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -7,4 +7,8 @@ export const tagColors: Record<string, string> = {
   daisy: "bg-blue-100 text-blue-700 border-blue-400 hover:bg-blue-300",
   millie: "bg-violet-100 text-violet-700 border-violet-400 hover:bg-violet-300",
   zany: "bg-green-100 text-green-700 border-green-400 hover:bg-green-300",
+
+  glodival: "bg-yellow-100 text-yellow-700 border-yellow-400 hover:bg-yellow-300",
+  gov: "bg-red-100 text-red-700 border-red-400 hover:bg-red-300",
+
 };
