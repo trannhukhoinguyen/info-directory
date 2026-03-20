e4d8f9a427aa1ee77d5f803f04b308861fbd263b
trann <trannhukhoinguyen@gmail.com>
2026-03-20 13:49:42 +0700

    update

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -3,9 +3,9 @@ export const tagColors: Record<string, string> = {
   default: "bg-gray-100 text-gray-900 border-gray-200 hover:bg-gray-200", // Màu mặc định
 
   elly: "bg-purple-300 text-purple-900 border-purple-500 hover:bg-purple-400",
-  cheryx: "bg-pink-300 text-pink-900 border-pink-500 hover:bg-pink-400",
+  cheryx: "bg-hotpink-300 text-hotpink-900 border-hotpink-500 hover:bg-hotpink-400",
   daisy: "bg-blue-300 text-blue-900 border-blue-500 hover:bg-blue-400",
-  millie: "bg-violet-300 text-violet-900 border-violet-500 hover:bg-violet-400",
+  millie: "bg-darkPink-300 text-darkPink-900 border-darkPink-500 hover:bg-darkPink-400",
   zany: "bg-green-300 text-green-900 border-green-500 hover:bg-green-400",
 
   glodival: "bg-yellow-300 text-yellow-900 border-yellow-500 hover:bg-yellow-400",
